1acf014b63d591660b3a7209dd8b9e996d60655b
Sara Garcia <saragca81@gmail.com>
2023-11-21 20:57:04 -0800

    refactored code

--- models/Profile.js
+++ models/Profile.js
@@ -1,11 +1,15 @@
-const { Schema, model} = require('mongoose');
+const { Schema, model } = require('mongoose');
 
 const profileSchema = new Schema({
     name: {
         type: String,
-        require: true,
+        required: true,
         unique: true,
         trim: true
-    }
-    
+    },
+    endoseCount: Number
 })
+
+const Profile = model('Profile', profileSchema)
+
+module.exports = Profile;
--- models/index.js
+++ models/index.js
@@ -1,3 +1,5 @@
 const Profile = require('./Profile');
 
+
+
 module.exports = { Profile}
