9451e84ed413d74202fb98556c76eeb5c57ab16f
Sara Garcia <saragca81@gmail.com>
2023-11-21 18:59:12 -0800

    updated app.js

--- public/src/app.js
+++ public/src/app.js
@@ -1,1 +1,28 @@
-console.log(`Learning during office hours`);
+let profileListEL = document.getElementById("profile-list")
+let btnProfile = document.getElementById("btnProfile")
+let profileNameEL = document.getElementById("profile-name");
+
+const addDisplay = (name, endorsedCount) => {
+   profileListEL.innerHTML +=`
+   <div class="col-12 col-xl-6">
+    <div class="card mb-3">
+    <h4 class="card-header bg-dark text-light p-2 m-0">${name} <br><span class="text-white"
+    style="font-size: 1rem;">currently has ${endorsedCount} endorsed
+    skills</span></h4><a class="btn btn-block btn-squared btn-light text-dark"
+href="/profiles/655c1322f5b34b60b39db3d1">View and endorse their
+skills.</a>
+</div>
+</div>
+
+   `
+}
+
+btnProfile.addEventListerner("click", (event) => {
+    event.preventDefault()
+
+
+  const profileName = profileNameEl.value;
+  addDisplay(profileName,300);
+
+    
+})
