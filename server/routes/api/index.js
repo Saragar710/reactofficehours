d9e6b3143b07544e768aaac8691d867618d8daa9
Sara Garcia <saragca81@gmail.com>
2023-11-23 08:42:24 -0800

    updated code to match Stanleys

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const apiRouter = require('./api')
+const profileRouter = require('./profile')
 
 
 //APIs
-router.use('/api',apiRouter)
+router.use('/profile',profileRouter)
 
 //views (Public)
 //router.use(express.static(__dirname + "/public"))
 
 //router.use(express.static(path.join(__dirname, '.../client/dist')))
-router.get(express.static(path.join(__dirname, '.../client/dist')))
+//router.get(express.static(path.join(__dirname, '.../client/dist')))
 
 module.exports = router;
